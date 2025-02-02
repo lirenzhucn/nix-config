185915acb50c799e55a9cc5cdfbc473bf703d9d8
Liren Zhu <liren.zhu.cn@gmail.com>
2025-02-02 15:53:39 -0800

    move configuration to its own module

---
+++ configuration.nix
@@ -0,0 +1,88 @@
+{ pkgs, inputs, ... }: {
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs;
+    [ fastfetch
+      tree
+      stow
+      wget
+      zoxide
+      git
+      neovim
+      ripgrep
+      lazygit
+      tmux
+      btop
+      ffmpeg
+      ffmpegthumbnailer
+      yazi
+      zig
+      fd
+      fzf
+      lua
+      jq
+      cloc
+      duf
+      # node stuff
+      nodejs
+      nodePackages.typescript
+      # hledger related stuff
+      hledger
+      hledger-ui
+      poppler_utils  # for `pdftotext` utility
+      # GUI apps
+      brave
+    ];
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Enable alternative shell support in nix-darwin.
+  programs.zsh.enable = true;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+
+  # The platform the configuration will be used on.
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  users.users.lirenzhu = {
+    name = "lirenzhu";
+    home = "/Users/lirenzhu";
+  };
+
+  # the option speaks for itself...
+  security.pam.enableSudoTouchIdAuth = true;
+
+  homebrew = {
+    enable = true;
+    onActivation.cleanup = "uninstall";
+    taps = [
+      "felixkratz/formulae"
+      "moritzsternemann/apple-fonts"
+      "nikitabobko/tap"
+      "homebrew/services"
+    ];
+    brews = [
+      "borders"
+      "ghcup"
+      "gnupg"
+      "go"
+      "imagemagick"
+      "pandoc"
+      "pipx"
+      "sketchybar"
+    ];
+    casks = [
+      "aerospace"
+      "ghostty"
+      "font-sf-pro"
+      "font-symbols-only-nerd-font"
+      "sf-symbols"
+    ];
+  };
+}
--- flake.nix
+++ flake.nix
@@ -8,100 +8,12 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.fastfetch
-          pkgs.tree
-          pkgs.stow
-          pkgs.wget
-          pkgs.zoxide
-          pkgs.git
-          pkgs.neovim
-          pkgs.ripgrep
-          pkgs.lazygit
-          pkgs.tmux
-          pkgs.btop
-          pkgs.ffmpeg
-          pkgs.ffmpegthumbnailer
-          pkgs.yazi
-          pkgs.zig
-          pkgs.fd
-          pkgs.fzf
-          pkgs.lua
-          pkgs.jq
-          pkgs.cloc
-          pkgs.duf
-          # hledger related stuff
-          pkgs.hledger
-          pkgs.hledger-ui
-          pkgs.poppler_utils  # for `pdftotext` utility
-          # GUI apps
-          pkgs.brave
-        ];
-      # services.aerospace.enable = true;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      programs.zsh.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      users.users.lirenzhu = {
-        name = "lirenzhu";
-        home = "/Users/lirenzhu";
-      };
-
-      # the option speaks for itself...
-      security.pam.enableSudoTouchIdAuth = true;
-
-      homebrew = {
-        enable = true;
-        onActivation.cleanup = "uninstall";
-        taps = [
-          "felixkratz/formulae"
-          "moritzsternemann/apple-fonts"
-          "nikitabobko/tap"
-          "homebrew/services"
-        ];
-        brews = [
-          "borders"
-          "ghcup"
-          "gnupg"
-          "go"
-          "imagemagick"
-          "node"
-          "pandoc"
-          "pipx"
-          "sketchybar"
-        ];
-        casks = [
-          "aerospace"
-          "ghostty"
-          "font-sf-pro"
-          "font-symbols-only-nerd-font"
-          "sf-symbols"
-        ];
-      };
-    };
-  in
   {
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#Lirens-MacBook-Pro
     darwinConfigurations."Lirens-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ ./configuration.nix ];
+      specialArgs = { inherit inputs; };
     };
   };
 }
